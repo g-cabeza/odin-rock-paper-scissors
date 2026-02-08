c0105a31d931fb0d7ab2eb294ca7426fc9653df3
Guillermo Cabeza <guillermo.f.cabeza@gmail.com>
2026-02-08 12:28:51 -0300

    playGame has 5 iterations now!
    playGame function to have 5 iterations has been solved.
    playRound has been nested within playGame.
    Every human and computer selection is printed.
    Missing score counter, needs to be solved.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-console.log("Hello World!");
+//console.log("Hello World!");
 
 // Variables
 
@@ -32,53 +32,97 @@ function getHumanChoice (){
 }
 
 
-function playRound (humanChoice, computerChoice) {
+function playGame(humanScore,computerScore) {
+
+
+
+    function playRound (humanChoice, computerChoice) {
 
     //Human chooses rock
     if (humanChoice == "ROCK" &&  computerChoice == "ROCK") {
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
         console.log("It's a draw! Rock can't beat Rock");
     }
     if (humanChoice == "ROCK" &&  computerChoice == "PAPER") {
-        console.log("You lose! Paper beats Rock")
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
+        console.log("You lose! Paper beats Rock");
     }
     if (humanChoice == "ROCK" &&  computerChoice == "SCISSORS") {
-       console.log("You win! Rock beats Scissors") 
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
+        console.log("You win! Rock beats Scissors");
     }
 
     //Human chooses paper
     if (humanChoice == "PAPER" &&  computerChoice == "ROCK") {
-        console.log("You win! Paper beats rock!")
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
+        console.log("You win! Paper beats rock!");
     }
     if (humanChoice == "PAPER" &&  computerChoice == "PAPER") {
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
         console.log("It's a draw! Paper can't beat Paper")
     }
     if (humanChoice == "PAPER" &&  computerChoice == "SCISSORS") {
-       console.log("You lose! Scissors beats Paper") 
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
+        console.log("You lose! Scissors beats Paper");
     }
 
     //Human chooses scissors
     if (humanChoice == "SCISSORS" &&  computerChoice == "ROCK") {
-        console.log("You lose! Rock beats Scissors")
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
+        console.log("You lose! Rock beats Scissors");
     }
     if (humanChoice == "SCISSORS" &&  computerChoice == "PAPER") {
-        console.log("You win! Scissors beats Paper")
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
+        console.log("You win! Scissors beats Paper");
     }
     if (humanChoice == "SCISSORS" &&  computerChoice == "SCISSORS") {
-       console.log("It's a draw! Scissors can't beat Scissors") 
+        console.log("Human Choice: ", humanChoice);
+        console.log("Computer Choice", computerChoice);
+        console.log("It's a draw! Scissors can't beat Scissors") 
     }
 
-}
+    }
+    
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound (humanChoice,computerChoice);
 
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound (humanChoice,computerChoice);
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound (humanChoice,computerChoice);
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound (humanChoice,computerChoice);
+
+    humanChoice = getHumanChoice();
+    computerChoice = getComputerChoice();
+    playRound (humanChoice,computerChoice);
+
+}
 //Console testing
 
 //console.log(getComputerChoice());
 //console.log(getHumanChoice());
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
 
-console.log("Human Choice: ", humanSelection);
-console.log("Computer Choice: ", computerSelection);
 
-playRound (humanSelection,computerSelection);
+//console.log("Human Choice: ", humanSelection);
+//console.log("Computer Choice: ", computerSelection);
+
+playGame(humanScore,computerScore);
+console.log("Human Score: ", humanScore);
+console.log("Computer Score", computerScore);
