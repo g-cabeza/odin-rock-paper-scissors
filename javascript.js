0da461ef3ef7e71cff3169f3bc35b2f61111cfe9
Guillermo Cabeza <guillermo.f.cabeza@gmail.com>
2026-02-08 11:52:55 -0300

    playRound function added
    The playRound function is completed.
    9 IF statements where used to cover all possible cases.
    the topUpper function was used to solve the input formatting.

--- javascript.js
+++ javascript.js
@@ -1,21 +1,84 @@
 console.log("Hello World!");
 
+// Variables
+
+let humanScore = 0;
+let computerScore = 0;
+
+// Functions
+
 function getComputerChoice (){
 
     let choice = Math.random ();
 
     if (choice >= 0 && choice <= 0.33) {
-        return "Rock"
+        return "ROCK"
     }
 
     if (choice > 0.33 && choice <= 0.66) {
-        return "Paper"
+        return "PAPER"
     }
 
     if (choice > 0.66 && choice <= 1) {
-        return "Scissors"
+        return "SCISSORS"
     }
 
 }
 
-console.log(getComputerChoice())
+function getHumanChoice (){
+    let choice = prompt("Select between: Rock, Paper and Scissors.","Enter your choice: ");
+    return choice.toUpperCase();
+
+}
+
+
+function playRound (humanChoice, computerChoice) {
+
+    //Human chooses rock
+    if (humanChoice == "ROCK" &&  computerChoice == "ROCK") {
+        console.log("It's a draw! Rock can't beat Rock");
+    }
+    if (humanChoice == "ROCK" &&  computerChoice == "PAPER") {
+        console.log("You lose! Paper beats Rock")
+    }
+    if (humanChoice == "ROCK" &&  computerChoice == "SCISSORS") {
+       console.log("You win! Rock beats Scissors") 
+    }
+
+    //Human chooses paper
+    if (humanChoice == "PAPER" &&  computerChoice == "ROCK") {
+        console.log("You win! Paper beats rock!")
+    }
+    if (humanChoice == "PAPER" &&  computerChoice == "PAPER") {
+        console.log("It's a draw! Paper can't beat Paper")
+    }
+    if (humanChoice == "PAPER" &&  computerChoice == "SCISSORS") {
+       console.log("You lose! Scissors beats Paper") 
+    }
+
+    //Human chooses scissors
+    if (humanChoice == "SCISSORS" &&  computerChoice == "ROCK") {
+        console.log("You lose! Rock beats Scissors")
+    }
+    if (humanChoice == "SCISSORS" &&  computerChoice == "PAPER") {
+        console.log("You win! Scissors beats Paper")
+    }
+    if (humanChoice == "SCISSORS" &&  computerChoice == "SCISSORS") {
+       console.log("It's a draw! Scissors can't beat Scissors") 
+    }
+
+}
+
+//Console testing
+
+//console.log(getComputerChoice());
+//console.log(getHumanChoice());
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+console.log("Human Choice: ", humanSelection);
+console.log("Computer Choice: ", computerSelection);
+
+playRound (humanSelection,computerSelection);
