2d65fe416f1c3148688f7f1e2fab6ea2d2c57a4e
Guillermo Cabeza <guillermo.f.cabeza@gmail.com>
2026-05-11 15:00:40 -0300

    Coming back after months
    Re-read all the code to understand it, and modified and finished the part of code that lets you know who won.
    Then created a piece of code for drew games.
    Tested and all worked!

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 //console.log("Hello World!");
 
 // Variables
+    let humanScore = 0;
+    let computerScore = 0;
+    let drawScore = 0;
+    let counter = 5;
 
-let humanScore = 0;
-let computerScore = 0;
 
 // Functions
 
@@ -32,27 +34,32 @@ function getHumanChoice (){
 }
 
 
-function playGame(humanScore,computerScore) {
+function playGame() {
 
+    
 
 
     function playRound (humanChoice, computerChoice) {
+    // 0 = lose , 1= win, 2= draw
 
     //Human chooses rock
     if (humanChoice == "ROCK" &&  computerChoice == "ROCK") {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
         console.log("It's a draw! Rock can't beat Rock");
+        return 2;
     }
     if (humanChoice == "ROCK" &&  computerChoice == "PAPER") {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
         console.log("You lose! Paper beats Rock");
+        return 0;
     }
     if (humanChoice == "ROCK" &&  computerChoice == "SCISSORS") {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
         console.log("You win! Rock beats Scissors");
+        return 1;
     }
 
     //Human chooses paper
@@ -60,16 +67,19 @@ function playGame(humanScore,computerScore) {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
         console.log("You win! Paper beats rock!");
+        return 1;
     }
     if (humanChoice == "PAPER" &&  computerChoice == "PAPER") {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
-        console.log("It's a draw! Paper can't beat Paper")
+        console.log("It's a draw! Paper can't beat Paper");
+        return 2;
     }
     if (humanChoice == "PAPER" &&  computerChoice == "SCISSORS") {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
         console.log("You lose! Scissors beats Paper");
+        return 0;
     }
 
     //Human chooses scissors
@@ -77,39 +87,92 @@ function playGame(humanScore,computerScore) {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
         console.log("You lose! Rock beats Scissors");
+        return 0;
     }
     if (humanChoice == "SCISSORS" &&  computerChoice == "PAPER") {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
         console.log("You win! Scissors beats Paper");
+        return 1;
     }
     if (humanChoice == "SCISSORS" &&  computerChoice == "SCISSORS") {
         console.log("Human Choice: ", humanChoice);
         console.log("Computer Choice", computerChoice);
-        console.log("It's a draw! Scissors can't beat Scissors") 
+        console.log("It's a draw! Scissors can't beat Scissors");
+        return 2; 
     }
 
     }
     
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
-    playRound (humanChoice,computerChoice);
+    roundResult = playRound (humanChoice,computerChoice);
+    if (roundResult == 0) {
+        computerScore = computerScore + 1;
+    }
+    if (roundResult == 1){
+        humanScore = humanScore +1;
+    }
+    if (roundResult == 2) {
+        drawScore = drawScore +1;
+    }
+
 
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
-    playRound (humanChoice,computerChoice);
+    roundResult = playRound (humanChoice,computerChoice);
+    if (roundResult == 0) {
+        computerScore = computerScore + 1;
+    }
+    if (roundResult == 1){
+        humanScore = humanScore +1;
+    }
+    if (roundResult == 2) {
+        drawScore = drawScore +1;
+    }
 
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
-    playRound (humanChoice,computerChoice);
+    roundResult = playRound (humanChoice,computerChoice);
+    if (roundResult == 0) {
+        computerScore = computerScore + 1;
+    }
+    if (roundResult == 1){
+        humanScore = humanScore +1;
+    }
+    if (roundResult == 2) {
+        drawScore = drawScore +1;
+    }
 
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
-    playRound (humanChoice,computerChoice);
+    roundResult = playRound (humanChoice,computerChoice);
+    if (roundResult == 0) {
+        computerScore = computerScore + 1;
+    }
+    if (roundResult == 1){
+        humanScore = humanScore +1;
+    }
+    if (roundResult == 2) {
+        drawScore = drawScore +1;
+    }
 
     humanChoice = getHumanChoice();
     computerChoice = getComputerChoice();
-    playRound (humanChoice,computerChoice);
+    roundResult = playRound (humanChoice,computerChoice);
+    if (roundResult == 0) {
+        computerScore = computerScore + 1;
+    }
+    if (roundResult == 1){
+        humanScore = humanScore +1;
+    }
+    if (roundResult == 2) {
+        drawScore = drawScore +1;
+    }
+
+    console.log("Human Score: ", humanScore);
+    console.log("Computer Score: ", computerScore);
+    console.log("Drew games: ", drawScore);
 
 }
 //Console testing
@@ -123,6 +186,4 @@ function playGame(humanScore,computerScore) {
 //console.log("Human Choice: ", humanSelection);
 //console.log("Computer Choice: ", computerSelection);
 
-playGame(humanScore,computerScore);
-console.log("Human Score: ", humanScore);
-console.log("Computer Score", computerScore);
+playGame();
